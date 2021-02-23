cf9c00ec63bd456db07a93a1653218f7ef440000
Daniel Atwood <daniel@atwood.io>
2021-02-22 21:16:07 -0500

    Create header partial

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,15 @@ module.exports = {
         enabled: process.env.HUGO_ENVIRONMENT === 'production',
         content: ['./layouts/**/*.html'],
     },
+    theme: {
+        textColor: theme => ({
+            ...theme('colors'),
+            'light': '#bac8d8',
+        }),
+        backgroundColor: theme => ({
+            ...theme('colors'),
+            'primary': '#0e4158',
+            'primary-dark': '#0d3345',
+        })
+    }
 }
